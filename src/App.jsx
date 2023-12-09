bf3386d24e3a48a74e7470bd4e362bf52a6daf7c
gustavo-lara <gustavolara63719@gmail.com>
2023-12-09 14:23:03 -0300

    Projeto devlist

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css"
+import CompletedTasks from "./components/CompletedTasks/CompletedTasks"
+import CreateNewTask from "./components/CreateNewTask/CreateNewTask"
+import Header from "./components/Header/Header"
+import Progress from "./components/Progress/Progress"
+import TodayTasks from "./components/TodayTasks/TodayTasks"
+
+
+const App = () => {
+  return (
+    <div className="container">
+    <Header/>
+    <section className="content">
+    <CreateNewTask/>
+    <TodayTasks/>
+    <div className="divider"/>
+    <Progress/>
+    <CompletedTasks/>
+    </section>
+    </div>
+  )
+}
+
+export default App
